3b362384d89b48ec542b9b6987a86dbdae893fa2
truongng1 <truong.m.nguyen@sjsu.edu>
2026-05-14 19:19:59 -0700

    add Visualizations

---
+++ Visualizations.R
@@ -0,0 +1,7 @@
+###############################################################################
+# MATH 167R - Final Project
+# Drug Consumption & Personality Traits Analysis
+# Group P: Kanishka Yadav, Krish Makwana, Eric Nguyen, Ramjot Dhillon
+# Spring 2026 
+# Instructor: Andrea Gottlieb
+###############################################################################
